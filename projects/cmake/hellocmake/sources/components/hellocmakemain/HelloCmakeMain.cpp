1c4b97df00e5fe707af1443c724fab8a5dfadff6
bensinghbeno <igenartist@gmail.com>
2017-04-20 19:53:50 +0200

    new cmake hello application structure

---
+++ projects/cmake/hellocmake/sources/components/hellocmakemain/HelloCmakeMain.cpp
@@ -0,0 +1,8 @@
+#include "HelloCmakeMain.h"
+
+int main(int argc, char* argv[])
+{
+    cout<<"HelloCmakeMain"<<endl;
+
+    return 0;
+}
---
+++ projects/cmake/hellocmake/sources/components/hellocmakemainapplication/HelloCmakeMainApplication.cpp
@@ -0,0 +1,13 @@
+#include "HelloCmakeMainApplication.h"
+
+HelloCmakeMainApplication::HelloCmakeMainApplication()
+{}
+
+~HelloCmakeMainApplication::HelloCmakeMainApplication()
+{}
+
+const bool HelloCmakeMainApplication::execute()
+{
+	cout<<"HelloCmakeMainApplication::execute()";
+	return true;
+}
