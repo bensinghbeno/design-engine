c2280e915c8d7eb6a7c52280d68dd5b74855248e
bensinghbeno <igenartist@gmail.com>
2017-06-19 13:34:42 +0000

    Python Opencv digit recognition

--- projects/cmake/my-cmake-project/build/app-hellocmake/CMakeFiles/app-hellocmake.dir/DependInfo.cmake
+++ projects/cmake/my-cmake-project/build/app-hellocmake/CMakeFiles/app-hellocmake.dir/DependInfo.cmake
@@ -1,23 +1,23 @@
 # The set of languages for which implicit dependencies are needed:
-SET(CMAKE_DEPENDS_LANGUAGES
+set(CMAKE_DEPENDS_LANGUAGES
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
-SET(CMAKE_DEPENDS_CHECK_CXX
-  "/home/bensingh/my/dev/design-engine/projects/cmake/my-cmake-project/modules/app-hellocmake/src/hellocmakemain/HelloCmakeMain.cpp" "/home/bensingh/my/dev/design-engine/projects/cmake/my-cmake-project/build/app-hellocmake/CMakeFiles/app-hellocmake.dir/src/hellocmakemain/HelloCmakeMain.cpp.o"
-  "/home/bensingh/my/dev/design-engine/projects/cmake/my-cmake-project/modules/app-hellocmake/src/hellocmakemainapplication/HelloCmakeMainApplication.cpp" "/home/bensingh/my/dev/design-engine/projects/cmake/my-cmake-project/build/app-hellocmake/CMakeFiles/app-hellocmake.dir/src/hellocmakemainapplication/HelloCmakeMainApplication.cpp.o"
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ben/design-engine/projects/cmake/my-cmake-project/modules/app-hellocmake/src/hellocmakemain/HelloCmakeMain.cpp" "/home/ben/design-engine/projects/cmake/my-cmake-project/build/app-hellocmake/CMakeFiles/app-hellocmake.dir/src/hellocmakemain/HelloCmakeMain.cpp.o"
+  "/home/ben/design-engine/projects/cmake/my-cmake-project/modules/app-hellocmake/src/hellocmakemainapplication/HelloCmakeMainApplication.cpp" "/home/ben/design-engine/projects/cmake/my-cmake-project/build/app-hellocmake/CMakeFiles/app-hellocmake.dir/src/hellocmakemainapplication/HelloCmakeMainApplication.cpp.o"
   )
-SET(CMAKE_CXX_COMPILER_ID "GNU")
-
-# Targets to which this target links.
-SET(CMAKE_TARGET_LINKED_INFO_FILES
-  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
-SET(CMAKE_C_TARGET_INCLUDE_PATH
-  "/home/bensingh/my/dev/design-engine/projects/cmake/my-cmake-project/modules/app-hellocmake/src/hellocmakemain"
-  "/home/bensingh/my/dev/design-engine/projects/cmake/my-cmake-project/modules/app-hellocmake/src/hellocmakemainapplication"
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/ben/design-engine/projects/cmake/my-cmake-project/modules/app-hellocmake/src/hellocmakemain"
+  "/home/ben/design-engine/projects/cmake/my-cmake-project/modules/app-hellocmake/src/hellocmakemainapplication"
   )
-SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
-SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
-SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ projects/tensorflow/hellotf.py
@@ -0,0 +1,3 @@
+import tensorflow as tf;
+
+print('Tensorflow version = %s' % tf.__version__)
