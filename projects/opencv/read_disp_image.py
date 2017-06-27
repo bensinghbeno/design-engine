d3063db82d84095af5a14ad011d7b46e6b9ee255
bensinghbeno <igenartist@gmail.com>
2017-06-27 16:30:04 +0200

    opencv added read and display image

---
+++ projects/opencv/read_disp_image.py
@@ -0,0 +1,16 @@
+import sys
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+if (len(sys.argv) == 1):
+    sys.exit('  No Arguments provided, Please provide image directory path')
+else:	
+	image_path = sys.argv[1];
+
+print('  image path = %s'%image_path);
+
+img = cv2.imread(image_path,0)
+plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
+plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+plt.show()
