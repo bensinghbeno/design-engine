8077b46a9d51e4afd2a2453879bc344b47539a47
Beno <bbeno@w2k.feico.com>
2017-08-15 17:04:31 +0200

    qt: added cppref

--- projects/qt/TestCpp/myconsole/main.cpp
+++ projects/qt/TestCpp/myconsole/main.cpp
@@ -9,7 +9,13 @@ int main(int argc, char* argv[])
 {
   cout<<"Hello myconsole tesapp"<<endl;
 
-
+    #if __cplusplus==201402L
+    std::cout << "C++14" << std::endl;
+    #elif __cplusplus==201103L
+    std::cout << "C++11" << std::endl;
+    #else
+    std::cout << "C++" << std::endl;
+    #endif
 
 
   return 0;
