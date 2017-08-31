becb534a67c7e840dfbb157865c66fb58340e56a
ben <bensinghbeno@gmail.com>
2017-08-31 06:44:54 -0700

    opencv-qt: split detected images - lazy

--- projects/opencv/qt-opencv/opencv_detect_image_box/opencv_detect_image_box/main.cpp
+++ projects/opencv/qt-opencv/opencv_detect_image_box/opencv_detect_image_box/main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <QString>
 
 using namespace cv;
 using namespace std;
@@ -12,6 +13,8 @@ int thresh = 100;
 int max_thresh = 255;
 RNG rng(12345);
 
+static std::string sourceFile;
+
 /// Function header
 void thresh_callback(int, void* );
 
@@ -20,6 +23,7 @@ int main( int argc, char** argv )
 {
   /// Load source image and convert it to gray
   src = imread( argv[1], 1 );
+  sourceFile = argv[1];
 
   /// Convert image to gray and blur it
   cvtColor( src, src_gray, CV_BGR2GRAY );
@@ -67,13 +71,21 @@ void thresh_callback(int, void* )
   for( int i = 0; i< contours.size(); i++ )
      {
        Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-       drawContours( src, contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
+       //drawContours( src, contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
        rectangle( src, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
        //circle( drawing, center[i], (int)radius[i], color, 2, 8, 0 );
+
+       // Copy rec area to seperate image
+       Mat img1, img2;
+       img1 = imread(sourceFile , 1 );
+       img2 = img1(Rect(boundRect.at(i).x, boundRect.at(i).y, boundRect.at(i).width, boundRect.at(i).height));
+       QString title("Detected No : ");
+       title += QString::number(i);
+       imshow( title.toStdString().c_str(), img2 );
      }
 
   /// Show in a window
   namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
-  imshow( "Contours", src );
+  //imshow( "Contours", src );
 }
 
