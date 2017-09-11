9479b80ff60650c483dda29ff0186a9fb752cd4f
bensinghbeno <igenartist@gmail.com>
2017-09-11 21:34:15 +0000

    perceptron: refactored

--- projects/neuralnetworks/qt/perceptron/ben_perceptron/main.cpp
+++ projects/neuralnetworks/qt/perceptron/ben_perceptron/main.cpp
@@ -7,14 +7,9 @@
 int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
+
   MainWindow w;
-
-
-  PerceptronWidget* pPerceptronOutPutWidget = new PerceptronWidget();
-  pPerceptronOutPutWidget->setMinimumSize(400,400);
-
-  w.setGeometry(1300,100,900,1000);
-  w.setCentralWidget(pPerceptronOutPutWidget);
   w.show();
+
   return a.exec();
 }
