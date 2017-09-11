704f67d5d0ac56323fdb1e87bb7defdd0c748d2e
bensinghbeno <igenartist@gmail.com>
2017-09-11 21:34:15 +0000

    perceptron: refactored

--- projects/neuralnetworks/qt/perceptron/ben_perceptron/mainwindow.cpp
+++ projects/neuralnetworks/qt/perceptron/ben_perceptron/mainwindow.cpp
@@ -2,14 +2,19 @@
 #include "ui_mainwindow.h"
 #include <QDebug>
 
-MainWindow::MainWindow(QWidget *parent) :
-  QMainWindow(parent),
-  ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent)
+  :  QMainWindow(parent)
+  ,  ui(new Ui::MainWindow)
+  ,  m_pPerceptronOutPutWidget(new PerceptronWidget(parent))
 {
   ui->setupUi(this);
+  m_pPerceptronOutPutWidget->setMinimumSize(400,400);
+  this->setGeometry(1300,100,900,1000);
+  this->setCentralWidget(m_pPerceptronOutPutWidget);
 }
 
 MainWindow::~MainWindow()
 {
   delete ui;
+  delete m_pPerceptronOutPutWidget;
 }
