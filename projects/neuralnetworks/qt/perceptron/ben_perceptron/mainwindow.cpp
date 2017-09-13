26718d399ad8bc2dd66d965b379db6ce153980d2
bensinghbeno <igenartist@gmail.com>
2017-09-13 21:29:00 +0000

    perceptron: added perceptron count

--- projects/neuralnetworks/qt/perceptron/ben_perceptron/mainwindow.cpp
+++ projects/neuralnetworks/qt/perceptron/ben_perceptron/mainwindow.cpp
@@ -10,16 +10,8 @@ MainWindow::MainWindow(QWidget *parent)
   ui->setupUi(this);
   m_pPerceptronOutPutWidget->setMinimumSize(400,400);
   this->setGeometry(1300,100,900,1000);
-//  QHBoxLayout* lay2 = new QHBoxLayout();
-//  QVBoxLayout* lay = new QVBoxLayout();
-//  QPushButton* btn1 = new QPushButton("btn1");
-//  QPushButton* btn2 = new QPushButton("btn1");
-//  lay2->addWidget(m_pPerceptronOutPutWidget);
-//  lay1->addWidget(btn1);
-//  lay1->addWidget(btn2);
-//  lay->addLayout(lay1);
-//  lay->addLayout(lay2);
-  this->setCentralWidget(m_pPerceptronOutPutWidget);
+  ui->verticalLayout->addWidget(m_pPerceptronOutPutWidget);
+  //this->setCentralWidget(m_pPerceptronOutPutWidget);
   centralWidget()->setMouseTracking(true);
   //installEventFilter(m_pPerceptronOutPutWidget->getInputLabel());
 }
