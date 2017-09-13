ac0ce0efb14db2d309c7345504c638c0fe14b79b
bensinghbeno <igenartist@gmail.com>
2017-09-13 20:39:58 +0000

    perceptron: hover connections grey border

--- projects/neuralnetworks/qt/perceptron/ben_perceptron/perceptronconnectors.cpp
+++ projects/neuralnetworks/qt/perceptron/ben_perceptron/perceptronconnectors.cpp
@@ -1,9 +1,11 @@
 #include "perceptronconnectors.h"
 
 PerceptronConnectors::PerceptronConnectors(QWidget *parent)
-  : QWidget(parent)
+  : QFrame(parent)
   , m_pConnectorsVLayout(new QVBoxLayout())
 {
   this->setLayout(m_pConnectorsVLayout);
+  this->setObjectName("myObject");
+  this->setStyleSheet("#myObject { border: 5px solid grey; }");
 }
 
