54e2eb69b2ca1decc737eacf83a1ff3d60e161db
bensinghbeno <igenartist@gmail.com>
2017-09-22 21:18:10 +0000

    perceptron: simple structure computation

--- projects/neuralnetworks/qt/perceptron/ben_perceptron/datamodel.cpp
+++ projects/neuralnetworks/qt/perceptron/ben_perceptron/datamodel.cpp
@@ -4,7 +4,8 @@
 DataModel::DataModel(QObject *parent)
   : QObject(parent)
   , m_PerceptronCount(0)
-  , m_vecPerceptronDataStructure(10, new PerceptronDataStructure) //TBM
+  , m_vecInputs(3, 0) //TBM
+  , m_vecOutputs(3, 0) //TBM
 {
   connect(this,SIGNAL(SgnPerceptronInputChanged()),this,SLOT(SltStartNetworkComputation()));
 }
@@ -12,7 +13,7 @@ DataModel::DataModel(QObject *parent)
 void DataModel::SltOnPerceptronInputSet(int aIndex, int aValue)
 {
   qDebug() << "DataModel::SltOnPerceptronInputSet aIndex = " << aIndex << "aValue = " << aValue;
-  m_vecPerceptronDataStructure[aIndex]->m_InputVal  = aValue;
+  m_vecInputs[aIndex]  = aValue;
   emit SgnPerceptronInputChanged();
 
 }
@@ -29,8 +30,20 @@ void DataModel::SltOnPerceptronCountSet(int aCount)
 void DataModel::SltStartNetworkComputation()
 {
   myconsoledisp << "SltStartNetworkComputation()";
-  //m_vecPerceptronDataStructure[aIndex]->m_OutPutVal = aValue;
-  //SgnPerceptronOutPutChanged(aIndex, m_vecPerceptronDataStructure[aIndex]->m_OutPutVal);
+
+  int i=0;
+  for(TVecPerceptronOutputs::iterator it = m_vecOutputs.begin(); it != m_vecOutputs.end(); it++)
+  {
+    //(it)[i] = m_vecInputs[i];
+    //emit SgnPerceptronOutPutChanged(i, (it)[i]);
+    myconsoledisp << "m_vecInputs[i] = " << m_vecInputs[i];
+
+
+
+   ++i;
+  }
+
+
 }
 
 
