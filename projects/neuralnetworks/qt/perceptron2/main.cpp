6f8683bd3c5aaeaf27ff200efac0364fc2820a29
ben <igenartist@gmail.com>
2017-11-15 07:19:17 -0800

    json to text

--- projects/neuralnetworks/qt/perceptron2/main.cpp
+++ projects/neuralnetworks/qt/perceptron2/main.cpp
@@ -3,18 +3,30 @@
 #include "perceptronmainwindow.h"
 
 #include <QJsonObject>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    PerceptronMainWindow* pPerceptronMainWindow = new PerceptronMainWindow();
+//    PerceptronMainWindow* pPerceptronMainWindow = new PerceptronMainWindow();
 
-    PerceptronWidget* myPerceptronWidget = new PerceptronWidget();
+//    PerceptronWidget* myPerceptronWidget = new PerceptronWidget();
 
-    pPerceptronMainWindow->addPerceptronWidget(*myPerceptronWidget);
+//    pPerceptronMainWindow->addPerceptronWidget(*myPerceptronWidget);
 
-    pPerceptronMainWindow->show();
+//    pPerceptronMainWindow->show();
+
+    QJsonObject jsonobj;
+    jsonobj["w00"] = "77";
+    jsonobj["w01"] = "100";
+
+    QJsonDocument jsonDoc(jsonobj);
+    QString strJson(jsonDoc.toJson(QJsonDocument::Compact));
+
+    qDebug() << strJson;
+
+
 
 
     return a.exec();
