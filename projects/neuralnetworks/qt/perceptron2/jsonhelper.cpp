992fa365e8161708757305135d0572def6c52637
ben <igenartist@gmail.com>
2017-12-14 22:38:18 -0800

    perceptron2: qt-> python init via json

--- projects/neuralnetworks/qt/perceptron2/jsonhelper.cpp
+++ projects/neuralnetworks/qt/perceptron2/jsonhelper.cpp
@@ -49,21 +49,21 @@ void JsonHelper::readJson(QString filepath)
     loadjsonfile(filepath);
     //qWarning() << val;
 
-    QJsonDocument jsondoc = QJsonDocument::fromJson(m_fileBuffer.toUtf8());
-    QJsonObject sett = jsondoc.object();
-    QJsonValue value = sett.value(QString("appName"));
-    QJsonObject item = value.toObject();
-    QJsonArray test = item["layer1"].toArray();
+    //QJsonDocument jsondoc = QJsonDocument::fromJson(m_fileBuffer.toUtf8());
+    //QJsonObject sett = jsondoc.object();
+    //QJsonValue value = sett.value(QString("appName"));
+    //QJsonObject item = value.toObject();
+    //QJsonArray test = item["layer1"].toArray();
     //qDebug() << "orig = " << test[2].toString();
 
 
 
-    QString buff2 =  jsondoc.toJson();
-    QJsonDocument jsondoc2 = QJsonDocument::fromJson(buff2.toUtf8());
-    QJsonObject sett2 = jsondoc2.object();
-    QJsonValue value2 = sett2.value(QString("appName"));
-    QJsonObject item2 = value2.toObject();
-    QJsonArray test2 = item2["layer1"].toArray();
+    //QString buff2 =  jsondoc.toJson();
+    //QJsonDocument jsondoc2 = QJsonDocument::fromJson(buff2.toUtf8());
+    //QJsonObject sett2 = jsondoc2.object();
+    //QJsonValue value2 = sett2.value(QString("appName"));
+    //QJsonObject item2 = value2.toObject();
+    //QJsonArray test2 = item2["layer1"].toArray();
     //qDebug() << "orig2 = " << test2[2].toString();
 }
 
