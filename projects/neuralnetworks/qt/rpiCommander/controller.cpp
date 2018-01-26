888b7da7cb444f3fbabdd2bc3a81af68f3377801
igene <igenartist@gmail.com>
2018-01-26 21:48:32 +0100

    LRFR Commands working

--- projects/neuralnetworks/qt/rpiCommander/controller.cpp
+++ projects/neuralnetworks/qt/rpiCommander/controller.cpp
@@ -8,6 +8,7 @@ Controller::Controller(LogicModel& logicModel, MainWindow& mainWindow, QObject *
     connect(m_MainWindow.ui->REVERSE,SIGNAL(clicked(bool)),&m_LogicModel,SLOT(sltSendCommand()));
     connect(m_MainWindow.ui->LEFT,SIGNAL(clicked(bool)),&m_LogicModel,SLOT(sltSendCommand()));
     connect(m_MainWindow.ui->RIGHT,SIGNAL(clicked(bool)),&m_LogicModel,SLOT(sltSendCommand()));
+    connect(m_MainWindow.ui->STOP,SIGNAL(clicked(bool)),&m_LogicModel,SLOT(sltSendCommand()));
 
 }
 
--- projects/neuralnetworks/qt/rpiCommander/logicmodel.cpp
+++ projects/neuralnetworks/qt/rpiCommander/logicmodel.cpp
@@ -24,8 +24,12 @@ void LogicModel::SendJsonCommand(QString strCommand)
     m_pPerceptronJsonModel->insertvalue("CMD_MOVE",strCommand);
     qDebug() << "send CMD_MOVE = " << m_pPerceptronJsonModel->getvalue("CMD_MOVE");
 
+    m_UdpData.clear();
     m_UdpData.append(m_pPerceptronJsonModel->getJsonStringbuffer().toStdString().c_str());
     m_UdpClient.m_UdpSocket.writeDatagram(m_UdpData, QHostAddress("192.168.2.167"), 7777);
+
+    qDebug() << "BUFFER = " << m_pPerceptronJsonModel->getJsonStringbuffer();
+
 }
 
 
