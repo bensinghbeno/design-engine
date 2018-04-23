bd1cbc7ad10aa8fa0bbc1157f09275c3de7a3614
igene <igenartist@gmail.com>
2018-04-23 18:32:24 +0200

    Initial Mnist Linear Model Working

--- projects/arduino/Ben_Arduino_SerialTest/Ben_Arduino_SerialTest.ino
+++ projects/arduino/Ben_Arduino_SerialTest/Ben_Arduino_SerialTest.ino
@@ -1,53 +1,35 @@
-/*
-  Software serial multple serial test
-
- Receives from the hardware serial, sends to software serial.
- Receives from software serial, sends to hardware serial.
-
- The circuit:
- * RX is digital pin 10 (connect to TX of other device)
- * TX is digital pin 11 (connect to RX of other device)
-
- Note:
- Not all pins on the Mega and Mega 2560 support change interrupts,
- so only the following can be used for RX:
- 10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
-
- Not all pins on the Leonardo and Micro support change interrupts,
- so only the following can be used for RX:
- 8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
-
- created back in the mists of time
- modified 25 May 2012
- by Tom Igoe
- based on Mikal Hart's example
-
- This example code is in the public domain.
-
- */
- 
 #include <SoftwareSerial.h>
 
-void setup()
-{
+SoftwareSerial mySerial(9, 10); // RX, TX
+
+void setup() {
   // Open serial communications and wait for port to open:
-  Serial.begin(57600);
-  while (!Serial)
-  {
+  Serial.begin(115200);
+  while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
 
-  Serial.println("setup() :: Serial Port Initialized!!");
 
-  delay(1000);
-  Serial.println("setup() :: Hello Arduino Serial Test");
+  Serial.println("Arduino Serial Connected!");
+
+  // set the data rate for the SoftwareSerial port
+  mySerial.begin(115200);
+  while (!mySerial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.println("Software Serial Connected!!");
+
+  mySerial.println("AT\r\n");
 }
 
-void loop()
-{
-  if (Serial.available())
-  {
-    Serial.write(Serial.read());
+void loop() { // run over and over
+  delay(500);
+    mySerial.write("AT\r\n");
+
+  if (mySerial.available()) {
+    Serial.write(mySerial.read());
+  }
+  if (Serial.available()) {
+    mySerial.write(Serial.read());
   }
 }
-
