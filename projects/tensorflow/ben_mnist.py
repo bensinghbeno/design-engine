aac5dba3630af0620add6597b28a31e2ca5f1641
igene <igenartist@gmail.com>
2018-05-02 03:45:34 -0700

    mnist:  specific dataset image prediction

--- projects/tensorflow/ben_mnist.py
+++ projects/tensorflow/ben_mnist.py
@@ -71,6 +71,9 @@ feed_dict_test = {x: data.test.images,
 
 
 def plot_images(images, cls_true, cls_pred=None):
+
+    print("len(images) = %s"%(len(images)))
+    print("len(cls_true) = %s"%(len(cls_true)))
     assert len(images) == len(cls_true) == 9
     
     # Create figure with 3x3 sub-plots.
@@ -96,6 +99,13 @@ def plot_images(images, cls_true, cls_pred=None):
     # Ensure the plot is shown correctly with multiple plots
     # in a single Notebook cell.
     plt.show()
+
+
+def disp_img(img):
+    plt.imshow(img, cmap='binary')
+
+
+
 
 batch_size = 100
 
@@ -190,14 +200,109 @@ def plot_weights():
 
 # Performance after 10 optimization iterations
 
-
-#print_accuracy()
-#plot_example_errors()
-
 optimize(num_iterations=10)
-print_accuracy()
-#plot_example_errors()
-#plot_weights()
+#print_accuracy()
+
+def plot_images2(pred_result, images, cls_true, cls_pred=None):
+
+    len_images = len(images)
+    len_cls_true = len(cls_true)
+    print("len(images) = %s"%len_images)
+    print("len(cls_true) = %s"%(len_cls_true))
+    assert len(images) == len(cls_true)
+    
+    # Create figure with 3x3 sub-plots.
+    fig, axes = plt.subplots(3, 3)
+    fig.subplots_adjust(hspace=0.3, wspace=0.3)
+
+    for i, ax in enumerate(axes.flat):
+        # Plot image.
+        if(i < len_images):
+            ax.imshow(images[i].reshape(img_shape), cmap='binary')
+
+            # Show true and predicted classes.
+            if cls_pred is None:
+                xlabel = "True: {0}".format(cls_true[i])
+            else:
+                xlabel = "True: {0}, Pred: {1}".format(cls_true[i], cls_pred[i])
+
+            ax.set_xlabel(xlabel)
+        
+            # Remove ticks from the plot.
+            ax.set_xticks([])
+            ax.set_yticks([])
+        else:
+            print("Exceeded index")
+        
+    # Ensure the plot is shown correctly with multiple plots
+    # in a single Notebook cell.
+    fig = plt.gcf()
+    fig.canvas.set_window_title(pred_result) 
+    plt.show()
+
+
+def disp_img(img):
+    plt.imshow(img, cmap='binary')
+
+def plot_example_true(dictonary,test_images,test_classes):
+    # Use TensorFlow to get a list of boolean values
+    # whether each test-image has been correctly classified,
+    # and a list for the predicted class of each image.
+    correct, cls_pred = session.run([correct_prediction, y_pred_cls],
+                                    feed_dict=dictonary)
+
+    pred_result = "PREDICTION_TRUE"
+
+    # Negate the boolean array.
+    incorrect = (correct == True)
+    print("len(incorrect) = %s"%(len(incorrect)))
+    
+    # Get the images from the test-set that have been
+    # incorrectly classified.
+    images = (test_images)[incorrect]
+    len_images = len(images)
+    print("len(images) = %s"%len_images)
+
+    if(len_images == 0):
+        print("INFO: PREDICTION_FALSE")
+        pred_result = "PREDICTION_FALSE"
+        incorrect = (correct == False)
+        images = (test_images)[incorrect]
+        len_images = len(images)
+        print("len(images) = %s"%len_images)
+    
+    # Get the predicted classes for those images.
+    cls_pred = cls_pred[incorrect]
+    print("len(cls_pred) = %s"%(len(cls_pred)))
+
+    # Get the true classes for those images.
+    cls_true = (test_classes)[incorrect]
+    print("len(cls_true) = %s"%(len(cls_true)))
+
+    #plt.imshow(images[0].reshape(28,28), cmap='binary')
+    #plt.xlabel("LABEL")
+    #plt.show()
+    
+    # Plot the first 9 images.
+    plot_images2(pred_result,
+                images=images,
+                cls_true=cls_true,
+                cls_pred=cls_pred)
+
+image_index = 101;
+test_images = data.test.images[(image_index-1):image_index]
+test_labels = data.test.labels[(image_index-1):image_index]
+test_classes = data.test.cls[(image_index-1):image_index]
+feed_dict_test2 = {x: test_images,
+                  y_true: test_labels,
+                  y_true_cls: test_classes}
+
+plot_example_true(feed_dict_test2,test_images,test_classes)
+
+
+
+#plt.imshow(data.test.images[0].reshape(28,28), cmap='binary')
+#plt.show()
 
 
 
