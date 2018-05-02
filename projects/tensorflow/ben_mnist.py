698cd3335677450bc96381d0c0f0e2bb9e66a9db
igene <igenartist@gmail.com>
2018-05-02 03:50:23 -0700

    mnist: versions added

--- projects/tensorflow/ben_mnist.py
+++ projects/tensorflow/ben_mnist.py
@@ -1,3 +1,8 @@
+# Works with the below configuration
+# Ubuntu 16.04 LTS 64 bit (Guest running on VmWare)
+# Tensorflow 1.7.0 
+# Python 2.7.12
+
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
