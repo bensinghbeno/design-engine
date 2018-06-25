a2f91cb8cc8fe16fb4ef13785400b7ae612e5821
igene <igenartist@gmail.com>
2018-06-25 07:25:44 -0700

    Conversion Script Ready

--- projects/tensorflow/convert2grayscale28.py
+++ projects/tensorflow/convert2grayscale28.py
@@ -16,18 +16,56 @@ from scipy.misc import imread, imsave, imresize, toimage
 
 
 
-print(" === Start Converion to Grayscale =====")
+print(" === Start Convertion to Grayscale =====")
 
-images_root_path = ""
+compressed_image_dataset_path = ""
 
 if (len(sys.argv) == 1):
-    print("Please provide images_root_path")
+    print("Please provide compressed_image_dataset_path")
     exit()
             
 else:
-    images_root_path = str(sys.argv[1])
+    compressed_image_dataset_path = str(sys.argv[1])
     
-print(" images_root_path = %s"%images_root_path)
+print(" compressed_image_dataset_path = %s"%compressed_image_dataset_path)
+
+
+sep = '.'
+dataset_rel_dir = compressed_image_dataset_path.split(sep, 1)[0]
+dataset_rel_dir = dataset_rel_dir.split("/",1)[1] 
+print("dataset_rel_dir = %s"%dataset_rel_dir) 
+
+training_dir = "my_training_data"
+os.system('mkdir -p ' + training_dir)
+
+print("Extract Images from archive =============")
+os.system('tar -xvf ' + compressed_image_dataset_path + ' -C ' + training_dir)
+print("Extract Images completed ================")
+
+images_root_path = training_dir + '/' + dataset_rel_dir
+print("images_root_path = %s"%images_root_path)
+
+
+
+
+# Create Resized Images Directory
+resized_images_dir = training_dir + '/' + dataset_rel_dir + '_resized'
+os.system('mkdir -p ' +  resized_images_dir)
+
+for i in range(10):
+    os.system('mkdir -p ' + resized_images_dir + '/' + str(i))
+
+
+# Create Greyed Images Directory
+
+greyed_images_dir = training_dir + '/' + dataset_rel_dir + '_greyed'
+os.system('mkdir -p ' +  greyed_images_dir)
+
+for i in range(10):
+    os.system('mkdir -p ' + greyed_images_dir + '/' + str(i))
+
+
+# Resize & convert to  Greyed Images
 
 dir_list = os.listdir(images_root_path)
 
@@ -42,12 +80,12 @@ for dir in dir_list:
        i = i+1
        print("Converting Found File = %s"%file)
        file2convert = files_dir + "/" + file
-       resized_file = " converted2grayscale28/resized/" + dir + "/" + dir +"_" + str(i) + ".jpg"
+       resized_file = " " + resized_images_dir + "/" + dir + "/" + dir +"_" + str(i) + ".jpg"
        command = "convert -resize 28X28! " + file2convert + resized_file
        print("Resize Conversion Command = %s"%command)
        os.system(command)
 
-       command = "convert " + resized_file + " -colorspace Gray" + " converted2grayscale28/gray/" + dir + "/" + dir +"_" + str(i)  + "_28" + "_gray"+ ".jpg"
+       command = "convert " + resized_file + " -colorspace Gray" + " " + greyed_images_dir + "/" + dir + "/" + dir +"_" + str(i)  + "_28" + "_gray"+ ".jpg"
        print("Gray Conversion Command = %s"%command)
        os.system(command)
 
