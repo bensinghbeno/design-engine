d725e902a12b3c75f831669f521aa9308bc2f599
igene <igenartist@gmail.com>
2018-07-08 00:15:05 +0200

    Http request parsing done

--- projects/arduino/esp/esp_wifi_ap_2/esp_wifi_ap_2.ino
+++ projects/arduino/esp/esp_wifi_ap_2/esp_wifi_ap_2.ino
@@ -20,6 +20,44 @@ String output4State = "off";
 const int output5 = 5;
 const int output4 = 4;
 
+String getValue(String data, char separator, int index)
+{
+    int found = 0;
+    int strIndex[] = { 0, -1 };
+    int maxIndex = data.length() - 1;
+
+    for (int i = 0; i <= maxIndex && found <= index; i++) {
+        if (data.charAt(i) == separator || i == maxIndex) {
+            found++;
+            strIndex[0] = strIndex[1] + 1;
+            strIndex[1] = (i == maxIndex) ? i+1 : i;
+        }
+    }
+    return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
+void setupWiFi()
+{
+  WiFi.mode(WIFI_AP);
+
+  // Do a little work to get a unique-ish name. Append the
+  // last two bytes of the MAC (HEX'd) to "Thing-":
+  uint8_t mac[WL_MAC_ADDR_LENGTH];
+  WiFi.softAPmacAddress(mac);
+  String macID = String(mac[WL_MAC_ADDR_LENGTH - 2], HEX) +
+                 String(mac[WL_MAC_ADDR_LENGTH - 1], HEX);
+  macID.toUpperCase();
+  String AP_NameString = "WirefreeBubble";
+
+  char AP_NameChar[AP_NameString.length() + 1];
+  memset(AP_NameChar, 0, AP_NameString.length() + 1);
+
+  for (int i=0; i<AP_NameString.length(); i++)
+    AP_NameChar[i] = AP_NameString.charAt(i);
+
+  WiFi.softAP(AP_NameChar, WiFiAPPSK);
+}
+
 void setup() {
   Serial.begin(115200);
   // Initialize the output variables as outputs
@@ -68,7 +106,21 @@ void loop(){
             client.println();
 
             // turns the GPIOs on and off
-            if (header.indexOf("GET /5/on") >= 0) {
+            if (header.indexOf("GET /GPS") >= 0)
+            {
+              Serial.println("RECEIVE GPS DATA :: ");
+              Serial.println("HEADER = " + header);
+              String val = "";
+              for( int i = 0; i < 10 ; ++i)
+              {
+                val = getValue(header, '-', i);
+                Serial.println("Value at : " + String(i) + " = " + val);
+                if(val == "FIN")
+                {
+                    break;
+                }
+              }                         
+            } else if (header.indexOf("GET /5/on") >= 0) {
               Serial.println("GPIO 5 on");
               output5State = "on";
               digitalWrite(output5, HIGH);
@@ -140,26 +192,6 @@ void loop(){
   }
 }
 
-void setupWiFi()
-{
-  WiFi.mode(WIFI_AP);
 
-  // Do a little work to get a unique-ish name. Append the
-  // last two bytes of the MAC (HEX'd) to "Thing-":
-  uint8_t mac[WL_MAC_ADDR_LENGTH];
-  WiFi.softAPmacAddress(mac);
-  String macID = String(mac[WL_MAC_ADDR_LENGTH - 2], HEX) +
-                 String(mac[WL_MAC_ADDR_LENGTH - 1], HEX);
-  macID.toUpperCase();
-  String AP_NameString = "WirefreeBubble";
-
-  char AP_NameChar[AP_NameString.length() + 1];
-  memset(AP_NameChar, 0, AP_NameString.length() + 1);
-
-  for (int i=0; i<AP_NameString.length(); i++)
-    AP_NameChar[i] = AP_NameString.charAt(i);
-
-  WiFi.softAP(AP_NameChar, WiFiAPPSK);
-}
 
 
