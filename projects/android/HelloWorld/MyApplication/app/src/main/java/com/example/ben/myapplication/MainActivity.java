fd44c44eeebf1e1aec3b5c239d168021dbc13228
igene <igenartist@gmail.com>
2018-07-15 16:48:09 +0200

    Get request working

--- projects/android/HelloWorld/MyApplication/app/src/main/java/com/example/ben/myapplication/MainActivity.java
+++ projects/android/HelloWorld/MyApplication/app/src/main/java/com/example/ben/myapplication/MainActivity.java
@@ -4,14 +4,50 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
-        TextView text = new TextView(this);
+        final TextView text = new TextView(this);
         text.setText("Hello World, Android via Wifi !!");
         setContentView(text);
+
+
+// Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://192.168.4.1/GPS-078-FIN--";
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        text.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                text.setText("That didn't work!");
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
     }
+
+
 }
