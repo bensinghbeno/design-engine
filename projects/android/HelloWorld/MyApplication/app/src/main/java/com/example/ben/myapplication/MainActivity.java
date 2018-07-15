92c859fa8e780e296719f17febd83343c6523acb
igene <igenartist@gmail.com>
2018-07-15 15:25:14 +0200

    Android Wifi Hello World

---
+++ projects/android/HelloWorld/MyApplication/app/src/main/java/com/example/ben/myapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.ben.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        TextView text = new TextView(this);
+        text.setText("Hello World, Android via Wifi !!");
+        setContentView(text);
+    }
+}
