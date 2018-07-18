ba7801cd6e00e9ef0f6d6f9da28bb6bdb87ca063
igene <igenartist@gmail.com>
2018-07-18 17:22:15 +0200

    Layout Based Hello World

--- projects/android/HelloWorld/MyApplication/app/src/main/java/com/example/ben/myapplication/MainActivity.java
+++ projects/android/HelloWorld/MyApplication/app/src/main/java/com/example/ben/myapplication/MainActivity.java
@@ -3,51 +3,24 @@ package com.example.ben.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.os.Bundle;
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+public class MainActivity extends Activity {
 
-import java.util.HashMap;
-import java.util.Map;
-
-public class MainActivity extends AppCompatActivity {
+    private TextView text2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        final TextView text = new TextView(this);
-        text.setText("Hello World, Android via Wifi !!");
-        setContentView(text);
+        setContentView(R.layout.activity_main);
 
-
-// Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="http://192.168.4.1/GPS-078-FIN--";
-
-// Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        text.setText("Response is: "+ response.substring(0,500));
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                text.setText("That didn't work!");
-            }
-        });
-
-// Add the request to the RequestQueue.
-        queue.add(stringRequest);
-
+        text2 = (TextView) findViewById(R.id.text2);
+        text2.setText("   Hellooooo !!  ");
     }
-
 
 }
