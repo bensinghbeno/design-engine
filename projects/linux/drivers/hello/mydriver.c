dd2b239e46d1ceeaee1f42585411af0559ee1832
igene <igenartist@gmail.com>
2018-11-18 12:54:33 +0100

    Coomunicate from Userland to Driver module

--- projects/linux/drivers/hello/mydriver.c
+++ projects/linux/drivers/hello/mydriver.c
@@ -7,13 +7,23 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
-int init_module(void)
+static int my_init_module(void)
 {
 	printk(KERN_INFO" === Hello linux driver === \n");
 	return 0;
 }
 
-void cleanup_module(void)
+static void my_exit_module(void)
 {
 	printk(KERN_INFO" === Goodbye linux driver === \n");
 }
+
+static void ben_func(void)
+{
+	printk(KERN_INFO" === ben_func linux driver === \n");	
+}
+
+EXPORT_SYMBOL(ben_func);
+
+module_init(my_init_module);
+module_exit(my_exit_module);
