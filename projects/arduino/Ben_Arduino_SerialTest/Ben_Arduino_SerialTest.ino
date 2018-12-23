4bf9754cabf166b0344e22932b1eec675b5c2746
igene <igenartist@gmail.com>
2018-12-23 14:10:20 +0530

    Android-arduino: usb serial Communication success

--- projects/arduino/Ben_Arduino_SerialTest/Ben_Arduino_SerialTest.ino
+++ projects/arduino/Ben_Arduino_SerialTest/Ben_Arduino_SerialTest.ino
@@ -1,41 +1,80 @@
 #include <SoftwareSerial.h>
 
 String a;
-const int output2 = 16;
+//const int output2 = 16;
 
 
 void setup() {
 
 Serial.begin(115200); // opens serial port, sets data rate
-Serial.println("Hello Arduino Serial\n");
-pinMode(output2, OUTPUT);
-digitalWrite(output2, LOW);
+//Serial.println("Hello Arduino Serial\n");
+//pinMode(output2, OUTPUT);
+//digitalWrite(output2, LOW);
 
 
 }
 
 void loop() {
 
-while(Serial.available()) {
+  Serial.println("Hello Android from Arduino !! .. \n");
+  delay(1000);
+  while(true)
+  {
+if(Serial.available()) {
 
+//Serial.println("Hello Android from Arduino !! .. \n");
+//
+//
 a= Serial.readString();// read the incoming data as string
-
-Serial.println(a);
+//
+//Serial.println(a);
 //a.trim();
-
+//
 if (a.equals("b"))
 {
-  Serial.println("Command:  GPIO16 ON ");
-  digitalWrite(output2, HIGH);
-
+  Serial.println("Command:  Recognized");
+//  digitalWrite(output2, HIGH);
+//
 }
 else
 {
-  Serial.println("Command: Default : GPIO16 OFF ");
-  digitalWrite(output2, LOW);
+  
+  Serial.println("Command:  NOT Recognized");
+
+//  Serial.println("Command: Default : GPIO16 OFF ");
+//  digitalWrite(output2, LOW);
 
 }
 
 }
 
+
+}  
 }
+
+//while(Serial.available()) {
+//
+//Serial.println("Hello Android from Arduino !! .. \n");
+//
+//
+//a= Serial.readString();// read the incoming data as string
+//
+//Serial.println(a);
+//a.trim();
+//
+//if (a.equals("b"))
+//{
+//  Serial.println("Command:  GPIO16 ON ");
+//  digitalWrite(output2, HIGH);
+//
+//}
+//else
+//{
+//  Serial.println("Command: Default : GPIO16 OFF ");
+//  digitalWrite(output2, LOW);
+
+//}
+//
+//}
+//
+//}
