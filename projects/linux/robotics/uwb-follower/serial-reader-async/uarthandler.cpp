9231e6e8c5cbcba4068d2b6ea8824d0a9b184726
bensingh.beno@toyota.com <bensingh.beno@toyota.com>
2022-11-16 16:45:49 -0500

    Forward-UWB Follower WiringPi

--- projects/linux/robotics/uwb-follower/serial-reader-async/uarthandler.cpp
+++ projects/linux/robotics/uwb-follower/serial-reader-async/uarthandler.cpp
@@ -1,10 +1,17 @@
 #include "uarthandler.h"
 #include <QDebug>
 #include <QThread>
+#include <wiringPi.h> // Include WiringPi library!
+
 
 UartHandler::UartHandler(int argumentCount, QStringList &argumentList, QObject *parent) : QObject(parent)
 {
     qDebug() << "Launch Serial Handler";
+    wiringPiSetupGpio(); // Initialize wiringPi -- using Broadcom pin numbers
+    
+    pinMode(20, OUTPUT);     // Set regular LED as output
+    pinMode(21, OUTPUT);     // Set regular LED as output
+
     mThread = new QThread();
     mSerialPortReader0 = new SerialPortReader("ttyUSB0");
     mSerialPortReader0->moveToThread(mThread);
@@ -50,26 +57,32 @@ void UartHandler::handleDataUpdate1(QString data)
     //qDebug() << "Range 1 :: "  << nRange1;
     
     float diff = (nRange0 - nRange1);
+    
+    qDebug() << "DIFF :: "  << diff;
 
     // // DIRECTION // //
-    if ((nRange0 > 2) || (nRange1 > 2))
+    if ((nRange0 > 2.5) || (nRange1 > 2.5))
     {
-        if (diff > 0.2)
+        if (diff > 0.33)
         {
             qDebug() << "LEFT ";
         }
-        else if (diff< (-(0.2)))
+        else if (diff< (-(0.33)))
         {
             qDebug() << "RIGHT";
         }
         else
         {
             qDebug() << "FORWARD";
+            digitalWrite(20, HIGH);  
+            digitalWrite(21, HIGH);    
         }
     }
     else
         {
             qDebug() << "STOP";
+            digitalWrite(20, LOW);  
+            digitalWrite(21, LOW); 
         }
 
 
