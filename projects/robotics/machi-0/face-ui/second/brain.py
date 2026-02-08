821b095880f3aef112065a67694f07501c4caa31
ben <bensinghbeno@gmail.com>
2026-02-08 16:52:44 -0500

    machi0: eyebrows added

--- projects/robotics/machi-0/face-ui/second/brain.py
+++ projects/robotics/machi-0/face-ui/second/brain.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
-import sys
+from flask_socketio import SocketIO
+import logging
+
+# Disable heavy logging to keep terminal clean
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
@@ -11,8 +15,10 @@ def index():
 
 @app.route('/emotion/<type>')
 def trigger_emotion(type):
+    print(f">>> Switching Looi to: {type}")
     socketio.emit('change_emotion', {'emotion': type})
-    return f"Status: {type}", 200
+    return f"Looi status: {type}\n", 200
 
 if __name__ == '__main__':
+    print("Looi Brain Active on http://localhost:5000")
     socketio.run(app, host='0.0.0.0', port=5000)
